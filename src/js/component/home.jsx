a9a2005fec0e1b8681912cdb124820131b1bd0c0
AlisonCastroM <alisoncastro7616@gmail.com>
2024-10-20 01:45:40 +0000

    primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	let [listaTareas, setListaTareas] = useState(["cocinar", "estudiar", "ordenar"]);
+	const[nuevaTarea, setNuevaTarea]= useState("")
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5">
+			<h1 className="text-center mt-5">Todo</h1>
+			<div className="mx-auto col-6">
+				<input type="text" className="form-control" placeholder="¿Que necesitas hacer?"
+				value={nuevaTarea}onChange={(evento) => {
+					setNuevaTarea(evento.target.value)
+				}}
+				onKeyUp={(evento) => {
+
+					if(evento.key== "Enter"){
+						setListaTareas([...listaTareas, nuevaTarea]);
+						setNuevaTarea("")
+					}
+
+					console.log(evento.key)
+					
+				}}
+				/>
+				<ul className="">
+
+					{listaTareas.map((item, index) => {
+						return (
+							<li key={index}>
+								{item} <i onClick= {()=>{
+								const aux =	listaTareas.filter((task, ind) =>{
+										return(ind != index)
+									})
+									setListaTareas(aux)
+
+								}} className="fa-light fa-x icono-oculto"></i>
+							</li>
+						)
+					})}
+
+				</ul>
+				<span>
+					{listaTareas.length} Items left
+				</span>
+
+			</div>
 		</div>
+
 	);
 };
 
